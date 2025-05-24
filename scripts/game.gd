21d63e35375c7432a8c1dc7dab1631922be541df
Victor Simon Paulo <22.00117-4@maua.br>
2025-05-23 22:41:40 -0300

    funcionalidades do player e inimigo desenvolvidas

---
+++ scripts/game.gd
@@ -0,0 +1,30 @@
+extends Node2D
+@export var enemy_scenes: Array[PackedScene] = []
+@onready var laser_container = $LaserContainer
+@onready var timer = $EnemySpawnTimer
+@onready var enemy_container = $EnemyContainer
+var player = null
+
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	player.laser_shot.connect(_on_player_laser_shot)
+	
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+
+func _on_player_laser_shot(laser_scene, location):
+	var laser = laser_scene.instantiate()
+	laser.global_position = location
+	laser_container.add_child(laser)
+
+
+func _on_enemy_spawn_timer_timeout():
+	var e = enemy_scenes.pick_random().instantiate()
+	e.global_position = Vector2(randf_range(100,1000),-100)
+	enemy_container.add_child(e)
